f1cdbcbaf111c8d3337d7b5fc629b394a8ff76f7
Anshu-Poonia <anshupoonia902005@gmail.com>
2026-06-19 17:18:31 +0530

    rate limiting implemented

--- backend/src/lib/env.js
+++ backend/src/lib/env.js
@@ -4,7 +4,7 @@ export const ENV = {
     PORT: process.env.PORT,
     MONGO_URI: process.env.MONGO_URI,
     JWT_SECRET: process.env.JWT_SECRET,
-    Node_ENV: process.env.NODE_ENV,
+    NODE_ENV: process.env.NODE_ENV,
     CLIENT_URL: process.env.CLIENT_URL,
     RESEND_API_KEY: process.env.RESEND_API_KEY,
     EMAIL_FROM: process.env.EMAIL_FROM,
@@ -12,4 +12,6 @@ export const ENV = {
     CLOUDINARY_CLOUD_NAME: process.env.CLOUDINARY_CLOUD_NAME,
     CLOUDINARY_API_KEY: process.env.CLOUDINARY_API_KEY,
     CLOUDINARY_API_SECRET: process.env.CLOUDINARY_API_SECRET,
+    ARCJET_KEY: process.env.ARCJET_KEY,
+    ARCJET_ENV: process.env.ARCJET_ENV,
 };
