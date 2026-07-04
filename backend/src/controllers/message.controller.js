8fd72bd9d6263bb0df189604d2793950eb47326f
Anshu-Poonia <anshupoonia902005@gmail.com>
2026-07-04 14:32:04 +0530

    real-time messages implementation

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -1,4 +1,5 @@
 import cloudinary from "../lib/cloudinary.js";
+import { getReceiverSocketId, io } from "../lib/socket.js";
 import Message from "../models/Message.js";
 import User from "../models/User.js";
 
@@ -70,7 +71,10 @@ export const sendMessage = async (req, res) => {
 
     await newMessage.save();
 
-    // todo: emit the message to the receiver using socket.io
+    const receiverSocketId = getReceiverSocketId(receiverId);
+    if (receiverSocketId) {
+      io.to(receiverSocketId).emit("newMessage", newMessage);
+    }
 
     res.status(201).json(newMessage);
   } catch (error) {
